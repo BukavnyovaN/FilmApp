7a3da164d5e974705615da939fe5365af38e2232
BukavnyovaN <nadezhda.bukavnyova.job@gmail.com>
2024-12-11 11:28:07 +0300

    feat: add staff information to the movie detail page;

---
+++ src/components/staff-card/staff-card.jsx
@@ -0,0 +1,18 @@
+import {Card} from 'antd';
+import './staff-card.css';
+
+export const StaffCard = ({id, nameRu, nameEn, posterUrl, professionText}) => {
+    return (
+        <Card
+            style={{ width: 165 }}
+            cover={<img alt={nameRu || nameEn} src={posterUrl} style={{maxHeight: '210px'}}/>}
+            key={id}
+        >
+            <div className='staff-card_info'>
+                <div className='staff-card_profession'>
+                    {nameRu || nameEn}
+                </div>
+            </div>
+        </Card>
+    )
+};
--- src/pages/details/details.jsx
+++ src/pages/details/details.jsx
@@ -10,10 +10,12 @@ import {HeartOutlined} from "@ant-design/icons";
 import {useSelector} from "react-redux";
 import './details.css';
 import {getRandomColor} from "../../utils/helpers/gerRandomColor";
+import {StaffCard} from "../../components/staff-card/staff-card";
 
 export function Details() {
     const { id } = useParams();
     const [movieDetails, setMovieDetails] = useState(null);
+    const [movieStaff, setMovieStaff] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
     const { isAuthenticated } = useSelector(state => state.auth);
@@ -21,15 +23,23 @@ export function Details() {
     useEffect(() => {
         const fetchMovieDetails = async () => {
             try {
-                const response = await axios.get(
-                    ENDPOINTS.BASE_URL + ENDPOINTS.FILMS + ENDPOINTS.ROOT + id,
-                    {
-                    headers: {
-                        'X-API-KEY': API_KEY,
-                    },
-                });
+                const [movieDetailsResponse, movieStaffResponse] = await Promise.all([
+                    axios.get(ENDPOINTS.BASE_URL + ENDPOINTS.FILMS + ENDPOINTS.ROOT + id, {
+                        headers: {
+                            'X-API-KEY': API_KEY,
+                        },
+                    }),
+                    axios.get(ENDPOINTS.STAFF + id, {
+                        headers: {
+                            'X-API-KEY': API_KEY,
+                        },
+                    }),
+                ]);
 
-                setMovieDetails(response.data);
+                const limitedStaff = movieStaffResponse.data.filter((staff) => staff.professionText === 'Актеры').slice(0, 5);
+                console.log(limitedStaff);
+                setMovieDetails(movieDetailsResponse.data);
+                setMovieStaff(limitedStaff);
             } catch (err) {
                 setError(err.message);
             } finally {
@@ -60,63 +70,82 @@ export function Details() {
                     />
                 }
             </div>
-            <div className='details-page_info'>
+            <div className='details-page_body'>
                 <Image
                     width={260}
                     src={movieDetails.posterUrl}
                     preview={false}
                     alt={movieDetails.nameOriginal}
                 />
-                <Descriptions style={{width: '65%' }} bordered={true} colon={false} layout={'horizontal'} column={1}>
-                    {
-                        movieDetails.ratingKinopoisk &&
-                        <Descriptions.Item label="Рейтинг Кинопоиск" labelStyle={{width: '20%'}}>
-                            <Tooltip title={movieDetails.ratingKinopoisk}>
-                                <Rate disabled
-                                      defaultValue={movieDetails.ratingKinopoisk}
-                                      allowHalf={true}
-                                      count={10}
-                                />
-                            </Tooltip>
-                        </Descriptions.Item>
-                    }
-                    {
-                        movieDetails.ratingImdb &&
-                        <Descriptions.Item label="Рейтинг IMDb" labelStyle={{width: '20%'}}>
-                            <Tooltip title={movieDetails.ratingImdb}>
-                                <Rate disabled
-                                      defaultValue={movieDetails.ratingImdb}
-                                      allowHalf={true}
-                                      count={10}
-                                />
-                            </Tooltip>
-                        </Descriptions.Item>
-                    }
-                    {
-                        movieDetails.countries.length &&
-                        <Descriptions.Item label="Страны" labelStyle={{width: '20%'}}>
-                            {movieDetails.countries.map(country => (
-                                <div key={country.country}>{country.country}</div>
-                            ))}
-                        </Descriptions.Item>
-                    }
-                    {
-                        movieDetails.genres.length &&
-                        <Descriptions.Item label="Жанры" labelStyle={{width: '20%'}}>
-                            <Flex gap="4px 0" wrap>
-                                {movieDetails.genres.map(genre => (
-                                    <Tag color={getRandomColor()} key={genre.genre}>{genre.genre}</Tag>
+                <div className='details-page_info'>
+                    <Descriptions bordered={true} colon={false} layout={'horizontal'} column={1}>
+                        {
+                            movieDetails.ratingKinopoisk &&
+                            <Descriptions.Item label="Рейтинг Кинопоиск" labelStyle={{width: '20%'}}>
+                                <Tooltip title={movieDetails.ratingKinopoisk}>
+                                    <Rate disabled
+                                          defaultValue={movieDetails.ratingKinopoisk}
+                                          allowHalf={true}
+                                          count={10}
+                                    />
+                                </Tooltip>
+                            </Descriptions.Item>
+                        }
+                        {
+                            movieDetails.ratingImdb &&
+                            <Descriptions.Item label="Рейтинг IMDb" labelStyle={{width: '20%'}}>
+                                <Tooltip title={movieDetails.ratingImdb}>
+                                    <Rate disabled
+                                          defaultValue={movieDetails.ratingImdb}
+                                          allowHalf={true}
+                                          count={10}
+                                    />
+                                </Tooltip>
+                            </Descriptions.Item>
+                        }
+                        {
+                            movieDetails.countries.length &&
+                            <Descriptions.Item label="Страны" labelStyle={{width: '20%'}}>
+                                {movieDetails.countries.map(country => (
+                                    <div key={country.country}>{country.country}</div>
                                 ))}
-                            </Flex>
-                        </Descriptions.Item>
-                    }
+                            </Descriptions.Item>
+                        }
+                        {
+                            movieDetails.genres.length &&
+                            <Descriptions.Item label="Жанры" labelStyle={{width: '20%'}}>
+                                <Flex gap="4px 0" wrap>
+                                    {movieDetails.genres.map(genre => (
+                                        <Tag color={getRandomColor()} key={genre.genre}>{genre.genre}</Tag>
+                                    ))}
+                                </Flex>
+                            </Descriptions.Item>
+                        }
+                        {
+                            movieDetails.description &&
+                            <Descriptions.Item label="Описание" labelStyle={{width: '20%'}}>
+                                {movieDetails.description}
+                            </Descriptions.Item>
+                        }
+                    </Descriptions>
                     {
-                        movieDetails.description &&
-                        <Descriptions.Item label="Описание" labelStyle={{width: '20%'}}>
-                            {movieDetails.description}
-                        </Descriptions.Item>
+                        Array.isArray(movieStaff) &&
+                        !!movieStaff.length &&
+                        <>
+                            <h3 className='details-page_staff-title'>Актёры</h3>
+                            <div className='details-page_staff'>
+                                {movieStaff.map((actor) =>
+                                    <StaffCard id={actor.id}
+                                               nameEn={actor.nameEn}
+                                               nameRu={actor.nameRu}
+                                               posterUrl={actor.posterUrl}
+                                               professionText={actor.professionText}
+                                    />
+                                )}
+                            </div>
+                        </>
                     }
-                </Descriptions>
+                </div>
             </div>
         </div>
     );
